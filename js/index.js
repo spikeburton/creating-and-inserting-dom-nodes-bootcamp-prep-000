e93136ab8c299242433bdaf9b01981cdab198e51
Spike Burton <ralphburton3@gmail.com>
2018-10-29 04:16:11 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var el = document.createElement('div');
+el.innerHTML = "Hello, DOM!";
+el.style.backgroundColor = '#f9f9f9';
+el.style.height = '200px';
+el.style.width = '200px';
+document.body.appendChild(el)
