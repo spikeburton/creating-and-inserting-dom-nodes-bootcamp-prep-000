cc17ed715fe9d61603cf343c59b8085803d9dee8
Spike Burton <ralphburton3@gmail.com>
2018-10-30 06:11:02 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -5,4 +5,8 @@ el.innerHTML = "Hello, DOM!";
 el.style.backgroundColor = '#f9f9f9';
 el.style.height = '200px';
 el.style.width = '200px';
+<<<<<<< HEAD
 document.body.appendChild(el)
+=======
+document.body.appendChild(el)
+>>>>>>> d2138cdfffc0fc076c83c2882a97e724869c437d
